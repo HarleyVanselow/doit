7a5f1c76d8ef70591a0d913e706e45e84525a450
Harley Vanselow <code@harleyv.net>
2024-04-26 22:50:42 -0400

    Discord integration

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+import flask
 import functions_framework
 
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
+
+DISCORD_PUBLIC_KEY = "9416d2be504b253e228d3149e29825294715d261c348d9c7e2618276bb1419c8"
+
+
+def verify_request(request: flask.Request):
+    # Your public key can be found on your application in the Developer Portal
+    verify_key = VerifyKey(bytes.fromhex(DISCORD_PUBLIC_KEY))
+
+    signature = request.headers["X-Signature-Ed25519"]
+    timestamp = request.headers["X-Signature-Timestamp"]
+    body = request.data.decode("utf-8")
+
+    try:
+        verify_key.verify(f"{timestamp}{body}".encode(), bytes.fromhex(signature))
+    except BadSignatureError:
+        flask.abort(401, "invalid request signature")
+
+
 @functions_framework.http
-def hello_http(request):
+def hello_http(request: flask.Request):
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
@@ -13,11 +34,9 @@ def hello_http(request):
     """
     request_json = request.get_json(silent=True)
     request_args = request.args
-
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return 'Greetings {}!'.format(name)
+    if request.path == "/":
+        return {"type": 1}
+    return {
+        "type":4,
+        "data": request_json
+    }
