3cbc1c9438f3b920eb516b99180f510cbdb061ae
Harley Vanselow <code@harleyv.net>
2024-05-19 21:16:18 -0400

    Test

--- .github/workflows/registerCommands.sh
+++ .github/workflows/registerCommands.sh
@@ -3,22 +3,22 @@ API_ENDPOINT="https://discord.com/api/v10/applications/1233603538651713666/comma
 
 # Directory containing JSON files
 JSON_DIR="/commands"
-
+echo "Checking for commands inside $JSON_DIR"
 # Iterate through each JSON file in the directory
 for file in "$JSON_DIR"/*.json; do
     # Check if file exists and is a regular file
     if [ -f "$file" ]; then
         # Read contents of the JSON file
         json_data=$(cat "$file")
-        echo "Calling for file $file" >> $GITHUB_OUTPUT
+        echo "Calling for file $file"
         # Make a POST request to Discord API
         response=$(curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bot ${{ secrets.DISCORD_BOT_TOKEN }}" -d "$json_data" "$API_ENDPOINT")
 
         # Check if the request was successful
         if [[ $response == *"success"* ]]; then
-            echo "Command from file $file successfully added." >> $GITHUB_OUTPUT
+            echo "Command from file $file successfully added."
         else
-            echo "Failed to add command from file $file. Response: $response" >> $GITHUB_OUTPUT
+            echo "Failed to add command from file $file. Response: $response"
         fi
     fi
 done
