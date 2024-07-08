f148f8c45c4579b5ceaf5825782d09b25578795e
Asic Chen <a19c97@hotmail.com>
2024-07-08 00:13:10 -0400

    Fix shortened response

--- main.py
+++ main.py
@@ -318,13 +318,14 @@ def call_gemini(data):
         user, prompt, "Gemini", response)
 
 
-def check_response(response, user):
-    cutoff_message = '[Use /more to see the rest of the response.]'
+def check_response(response, user, prompt):
+    cutoff_message = '... [Use /more to see the rest of the response.]'
 
     # Check that response is within 2000 characters
-    if len(response) + len(cutoff_message) > 2000:
+    extra_len = len(user) + len(prompt) + len(cutoff_message) + 10
+    if len(response) + extra_len > 2000:
         print('Original response too long; displaying first 2000 characters and logging the rest in db.')
-        resp_short = response[:2000 - len(cutoff_message)] + cutoff_message
+        resp_short = response[:2000 - extra_len] + cutoff_message
         assert len(resp_short) <= 2000
 
         # Log the rest of the response to database
